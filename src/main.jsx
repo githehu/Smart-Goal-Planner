09763c1c0f12cbf57c6a6a7fc8cda86f056a84ef
githehu <githehudavid888@gmail.com>
2025-09-04 00:21:10 +0300

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Include your global styles (if you have any)
+import './style.css'; // Include your global styles (if you have any)
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
